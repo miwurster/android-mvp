dbc77d3e60b649ef5961b8790b9861a8dea2370d
Daniel Joos <daniel@joosweb.de>
2015-01-02 11:39:35 +0100

    Yeah, map click works

--- app-weather/app/src/main/java/inf/msc/yawapp/map/MapActivity.java
+++ app-weather/app/src/main/java/inf/msc/yawapp/map/MapActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.Arrays;
 import java.util.List;
@@ -14,12 +16,13 @@ import javax.inject.Inject;
 import inf.msc.yawapp.R;
 import inf.msc.yawapp.common.BaseModuleActivity;
 
-public class MapActivity extends BaseModuleActivity implements GoogleMap.OnMapLongClickListener {
+public class MapActivity extends BaseModuleActivity implements GoogleMap.OnMapLongClickListener, GoogleMap.OnMarkerClickListener {
 
     @Inject
     MapPresenter presenter;
 
     private MapFragment mapFragment;
+    private Marker mapMarker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +32,7 @@ public class MapActivity extends BaseModuleActivity implements GoogleMap.OnMapLo
 
         mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMap().setOnMapLongClickListener(this);
+        mapFragment.getMap().setOnMarkerClickListener(this);
     }
 
     @Override
@@ -38,6 +42,17 @@ public class MapActivity extends BaseModuleActivity implements GoogleMap.OnMapLo
 
     @Override
     public void onMapLongClick(LatLng latLng) {
+        if (mapMarker != null) {
+            mapMarker.remove();
+        }
+        mapMarker = mapFragment.getMap().addMarker(new MarkerOptions().position(latLng));
+
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        LatLng latLng = marker.getPosition();
         presenter.submitSearch((float) latLng.latitude, (float) latLng.longitude);
+        return true;
     }
 }
