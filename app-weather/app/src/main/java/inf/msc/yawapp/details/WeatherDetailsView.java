093f9e653decbbfec9d6214e9a1f85ea51203eec
Daniel Joos <daniel@joosweb.de>
2015-01-11 19:18:16 +0100

    Added source code headers

--- app-weather/app/src/main/java/inf/msc/yawapp/details/WeatherDetailsView.java
+++ app-weather/app/src/main/java/inf/msc/yawapp/details/WeatherDetailsView.java
@@ -1,3 +1,19 @@
+/*
+ * This source code is part of the research paper
+ * "Applying Model-View-Presenter Architecture to the Android Framework with App Prototype"
+ *
+ * Software Architecture and Management,
+ * Herman-Hollerith-Zentrum, Reutlingen University
+ *
+ * Authors:  Dennis G. Geisse,
+ *           Iven John,
+ *           Daniel Joos,
+ *           Sebastian Kotstein,
+ *           Michael Wurster
+ *
+ * Year:     2015
+ */
+
 package inf.msc.yawapp.details;
 
 import inf.msc.yawapp.model.WeatherData;
